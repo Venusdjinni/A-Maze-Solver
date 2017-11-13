2e3cdea389859e2ce1cff7f8e01194b6834183d3
Venusdjinni <arnold.wabo@gmail.com>
2017-11-13 09:19:45 +0100

    Blabla car

---
+++ Projet 1 IA/src/com/venus/app/NoeudAS.java
@@ -0,0 +1,16 @@
+package com.venus.app;
+
+/**
+ * Created by arnold on 13/11/17.
+ */
+public class NoeudAS extends Noeud {
+
+
+    public NoeudAS(Couple c) {
+        super(c);
+    }
+
+    public NoeudAS(Couple c, TypeNoeud t) {
+        super(c, t);
+    }
+}
